6108494f699ca5e2d38f5550e619a43b778f0079
kamesan <47214420+kamesan1577@users.noreply.github.com>
2023-10-11 01:52:45 +0900

    bug fix

--- src/gpt.js
+++ src/gpt.js
@@ -1,89 +1,91 @@
 const Completion = class {
-  constructor() {
-    this.END_POINT = "https://api.openai.com/v1";
-  }
+    constructor() {
+        this.END_POINT = "https://api.openai.com/v1";
+    }
 
-  async moderateNote(prompt, user_id) {
-    const BASE_URL =
-      "https://b0861yd058.execute-api.us-east-1.amazonaws.com/dev/";
-    const END_POINT = BASE_URL + "moderations";
-    console.log(prompt, user_id);
-    const response = await fetch(END_POINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        user_id: user_id,
-      }),
-    });
+    async moderateNote(prompt, user_id) {
+        const BASE_URL =
+            "https://b0861yd058.execute-api.us-east-1.amazonaws.com/dev/";
+        const END_POINT = BASE_URL + "moderations";
+        console.log(prompt, user_id);
+        try {
+            const response = await fetch(END_POINT, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    prompt: prompt,
+                    user_id: user_id,
+                }),
+            });
+            const data = await response.json();
+            console.log(data);
+            return data.response;
+        } catch (error) {
+            console.log(error);
+            return prompt;
+        }
 
-    const data = await response.json();
-    console.log(data);
-    return data.response;
-  }
-  async moderateReply(prompt, user_id) {
-    //リプライの修正機能を書く
-  }
+    }
+    async moderateReply(prompt, user_id) {
+        //リプライの修正機能を書く
+    }
 };
 
 class ModerateWithLlmButton {
-  constructor(textArea) {
-    this.textArea = textArea;
-    this.button = document.createElement("button");
-    this.buttonName = "修正";
-    this.button.textContent = this.buttonName;
-    this.button.className = "exButton";
-    this.button.disabled = true;
+    constructor(textArea) {
+        this.textArea = textArea;
+        this.button = document.createElement("button");
+        this.buttonName = "修正";
+        this.button.textContent = this.buttonName;
+        this.button.className = "exButton";
+        this.button.disabled = true;
 
-  }
-
-  async init() {
-    const img = document.createElement("img");
-    img.src = chrome.runtime.getURL("public/parrot.gif");
-    img.style.display = "none";
-    img.style.width = "20px";
-    img.style.height = "20px";
-    this.button.appendChild(img); // 初期化
-
-    const userId = await this.setUser();
-    this.button.addEventListener("click", async () => {
-      this.textArea.disabled = true;
-      this.button.disabled = true;
-      this.button.textContent = "";
-      img.style.display = "block";
-      this.button.appendChild(img);
-
-      try {
-        const completion = new Completion();
-        const newText = await completion.moderateNote(this.textArea.value, userId);
-        this.textArea.value = newText;
-      } catch (error) {
-        console.log(error);
-      }
-
-      this.textArea.dispatchEvent(new Event("input", { bubbles: true }));
-      this.textArea.disabled = false;
-      this.button.disabled = false;
-      this.button.textContent = this.buttonName;
-      img.style.display = "none";
-    });
-  }
-
-  async setUser() {
-    const userId = await chrome.storage.local.get(["jisshu-user-id"]);
-    console.log(userId);
-    if (userId === undefined) {
-      const uuid = crypto.randomUUID();
-      await chrome.storage.local.set({ "jisshu-user-id": uuid });
-      return this.setUser();
-    } else {
-      return userId["jisshu-user-id"];
     }
-  }
 
-  setButtonDisabled(isDisabled) {
-    this.button.disabled = isDisabled;
-  }
+    async init() {
+        const img = document.createElement("img");
+        img.src = chrome.runtime.getURL("public/parrot.gif");
+        img.style.display = "none";
+        img.style.width = "20px";
+        img.style.height = "20px";
+        this.button.appendChild(img); // 初期化
+
+        const userId = await this.setUser();
+        this.button.addEventListener("click", async () => {
+            this.textArea.disabled = true;
+            this.button.disabled = true;
+            this.button.textContent = "";
+            img.style.display = "block";
+            this.button.appendChild(img);
+
+            const completion = new Completion();
+            const newText = await completion.moderateNote(this.textArea.value, userId);
+            this.textArea.value = newText;
+
+
+            this.textArea.dispatchEvent(new Event("input", { bubbles: true }));
+            this.textArea.disabled = false;
+            this.button.disabled = false;
+            this.button.textContent = this.buttonName;
+            img.style.display = "none";
+        });
+    }
+
+    async setUser() {
+        const userId = await chrome.storage.local.get(["jisshu-user-id"]);
+        console.log(userId);
+        if (typeof userId["jisshu-user-id"] === "undefined") {
+            const uuid = crypto.randomUUID();
+            await chrome.storage.local.set({ "jisshu-user-id": uuid });
+            return this.setUser();
+        } else {
+            return userId["jisshu-user-id"];
+        }
+    }
+
+    setButtonDisabled(isDisabled) {
+        this.button.disabled = isDisabled;
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,43 +1,43 @@
 const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.addedNodes.length) {
-      mutation.addedNodes.forEach((node) => {
-        if (node.querySelector) {
-          const textArea = node.querySelector(
-            "textarea[data-cy-post-form-text]"
-          );
-          // console.log(textArea);
-          if (textArea) {
-            const buttonWrapper = document.createElement("div");
-            buttonWrapper.className = "button-wrapper";
-            const buttons = [new ModerateWithLlmButton(textArea)];
-            for (const button of buttons) {
-              button.init();
-            }
-            for (const button of buttons) {
-              buttonWrapper.appendChild(button.button);
-            }
-            const form = node.querySelector("footer[class=xkr7J]");
-            // console.log(form);
-            if (form) {
-              form.appendChild(buttonWrapper);
-            }
+    mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length) {
+            mutation.addedNodes.forEach((node) => {
+                if (node.querySelector) {
+                    const textArea = node.querySelector(
+                        "textarea[data-cy-post-form-text]"
+                    );
+                    // console.log(textArea);
+                    if (textArea) {
+                        const buttonWrapper = document.createElement("div");
+                        buttonWrapper.className = "button-wrapper";
+                        const buttons = [new ModerateWithLlmButton(textArea)];
+                        for (const button of buttons) {
+                            button.init();
+                        }
+                        for (const button of buttons) {
+                            buttonWrapper.appendChild(button.button);
+                        }
+                        const form = node.querySelector("footer[class=xkr7J]");
+                        // console.log(form);
+                        if (form) {
+                            form.appendChild(buttonWrapper);
+                        }
 
-            // テキストエリアに文字が入力された時にボタンを活性化
-            textArea.addEventListener("input", () => {
-              buttons.forEach((button) => {
-                button.setButtonDisabled(!textArea.value.trim()); // テキストが空でない場合は活性化
-              });
+                        // テキストエリアに文字が入力された時にボタンを活性化
+                        textArea.addEventListener("input", () => {
+                            buttons.forEach((button) => {
+                                button.setButtonDisabled(!textArea.value.trim()); // テキストが空でない場合は活性化
+                            });
+                        });
+                    }
+                }
             });
-          }
         }
-      });
-    }
-  });
+    });
 });
 
 observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-  characterData: true,
+    childList: true,
+    subtree: true,
+    characterData: true,
 });
