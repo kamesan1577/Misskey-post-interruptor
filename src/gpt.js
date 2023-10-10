a4777a69b71332138d4f02a1c0047759c985af0f
kamesaniniad <s1F102101042@iniad.org>
2023-10-10 20:15:52 +0900

    ボタン押したときに要素を非活性化

--- src/gpt.js
+++ src/gpt.js
@@ -1,92 +1,73 @@
 const Completion = class {
-    constructor() {
-        this.systemPrompt = "あなたにはユーザーのツイート内容が入力されます。入力に含まれる不適切な表現を柔らかい表現に置き換えてください。返答は変換結果のみを返してください。";
-        this.END_POINT = "https://api.openai.com/v1";
-    }
-    // 本家のAPIを叩く(多分もう使わない)
-    async sendPrompt(prompt) {
-        const API_KEY = await chrome.storage.local.get(["token"]);
-        const END_POINT = this.END_POINT;
-        const systemPrompt = this.systemPrompt;
-        const userPrompt = prompt;
-        console.log(userPrompt);
-        const response = await fetch(`${END_POINT.endpoint}/chat/completions`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${API_KEY.token}`,
-            },
-            body: JSON.stringify({
-                "model": "gpt-3.5-turbo",
-                "messages": [{
-                    "role": "system",
-                    "content": systemPrompt,
-                },
-                {
-                    "role": "user",
-                    "content": userPrompt,
-                }],
-            }),
-        });
-        const data = await response.json();
-        console.log(data);
-        return data.choices[0].message.content;
-    }
-    // TODO: 誰でも使えるので何かしらの制限をかける
-    // AWS lambda経由でAI-MOPを叩く
-    async sendPromptWithWrapper(prompt, model = "gpt-3.5-turbo") {
-        const END_POINT = "https://95nbyqlsr2.execute-api.us-east-1.amazonaws.com/test";
-        const userPrompt = prompt;
-        console.log(userPrompt);
-        const response = await fetch(`${END_POINT}?prompt=${userPrompt}&model=${model}`);
-        let data = await response.json();
+  constructor() {
+    this.END_POINT = "https://api.openai.com/v1";
+  }
 
-        //余計な'"'の削除
-        if (data[0] == '"') {
-            data = data.slice(1);
-        }
-        if (data[data.length - 1] == '"') {
-            data = data.slice(0, -1);
-        }
+  async moderateNote(prompt, user_id) {
+    const BASE_URL =
+      "https://b0861yd058.execute-api.us-east-1.amazonaws.com/dev/";
+    const END_POINT = BASE_URL + "moderations";
+    console.log(prompt);
+    const response = await fetch(END_POINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        user_id: user_id,
+      }),
+    });
 
-        console.log(data);
-        return data;
-    }
-    async moderateNote(prompt, user_id) {
-        const END_POINT = "https://s51ue6kvx7.execute-api.us-east-1.amazonaws.com/dev/";
-        console.log(prompt);
-        const response = await fetch(END_POINT, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "prompt": prompt,
-                "user_id": user_id,
-            })
-        })
-        let data = await response.json();
-
-        console.log(data);
-        return data;
-    }
-    async moderateReply(prompt, user_id) {
-        //リプライの修正機能を書く
-    }
-}
+    const data = await response.json();
+    console.log(data);
+    return data.response;
+  }
+  async moderateReply(prompt, user_id) {
+    //リプライの修正機能を書く
+  }
+};
 
 const ModerateWithLlmButton = class {
-    constructor(textArea) {
-        this.button = document.createElement("button");
-        this.buttonName = "修正";
-        this.buttonValue = textArea.value;
-        this.button.textContent = this.buttonName;
-        this.button.className = "exButton";
-        this.button.addEventListener("click", async () => {
+  constructor(textArea) {
+    this.button = document.createElement("button");
+    this.buttonName = "修正";
+    this.buttonValue = textArea.value;
+    this.button.textContent = this.buttonName;
+    this.button.className = "exButton";
+    this.button.disabled = true;
+    // img要素の作成
+    const img = document.createElement("img");
+    img.src = chrome.runtime.getURL("public/parrot.gif");
+    img.style.display = "none";
+    img.style.width = "20px";
+    img.style.height = "20px";
+    
+    this.button.appendChild(img);
+
+    // this.button.style.display = "none";
+
+    this.button.addEventListener("click", async () => {
+        textArea.disabled = true;
+        this.button.disabled = true;
+        this.button.textContent = "";
+        img.style.display = "block";
+        this.button.appendChild(img);
+        
+        try {
             const completion = new Completion();
             const newText = await completion.moderateNote(textArea.value, "test");
             textArea.value = newText;
-            textArea.dispatchEvent(new Event("input", { bubbles: true }));
-        });
+        } catch (error) {}
+
+        textArea.dispatchEvent(new Event("input", { bubbles: true }));
+        textArea.disabled = false;
+        this.button.disabled = false;
+        this.button.textContent = this.buttonName;
+        img.style.display = "none";
+    });
     }
-}
+    setButtonDisabled(isDisabled) {
+    this.button.disabled = isDisabled;
+    }
+};
